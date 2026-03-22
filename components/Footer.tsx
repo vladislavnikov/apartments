85f8497910a57e0e14503a38df9401e896c978bd
Vladislav <114019283+vladislavnikov@users.noreply.github.com>
2026-03-22 18:19:51 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,13 +13,15 @@ export default function Footer() {
     <footer>
       <div className="pt-6 sm:pt-8 md:pt-10 bg-no-repeat bg-cover bg-center bg-[url('/footer.jpg')]">
         <div className="mx-auto w-[92%] max-w-2xl sm:max-w-3xl md:max-w-4xl px-4 sm:px-8 md:px-12 bg-black/60 pt-6 pb-2 rounded-t-lg">
-          <div className="flex flex-col sm:flex-row justify-center gap-6 sm:gap-12 md:gap-20 text-xs sm:text-sm text-white">
+          <div className="flex flex-col sm:flex-row justify-center gap-6 sm:gap-12 md:gap-20 text-base sm:text-base text-white">
             <div className="text-center">
-              <h3 className="font-semibold text-sm sm:text-base mb-1 italic">
+              <h3 className="font-semibold text-base sm:text-base mb-1 italic">
                 {locale === 'en' ? 'Contact Us' : 'Свържете се с нас'}
               </h3>
-              <p className="text-xs sm:text-sm break-all sm:break-normal">office@todorov-co.com</p>
-              <p className="text-xs sm:text-sm">+359 89 910 0537</p>
+              <p className="text-base sm:text-base break-all sm:break-normal">
+                office@todorov-co.com
+              </p>
+              <p className="text-base sm:text-base">+359 89 910 0537</p>
               <div className="flex gap-2 justify-center mt-1">
                 <a
                   href="https://www.facebook.com/profile.php?id=61578379520805"
@@ -41,18 +43,20 @@ export default function Footer() {
             </div>
 
             <div className="text-center">
-              <h3 className="font-semibold text-sm sm:text-base mb-1 italic">
+              <h3 className="font-semibold text-base sm:text-base mb-1 italic">
                 {locale === Locale.EN ? 'Address' : 'Адрес'}
               </h3>
               <p>{locale === Locale.EN ? '1 Mostova Street' : 'ул. „Мостова“ 1'}</p>
-              <p className="text-xs sm:text-sm">
+              <p className="text-base sm:text-base">
                 {locale === Locale.EN ? 'Plovdiv, 4000' : 'Пловдив, 4000'}
               </p>
-              <p className="text-xs sm:text-sm">{locale === Locale.EN ? 'Bulgaria' : 'България'}</p>
+              <p className="text-base sm:text-base">
+                {locale === Locale.EN ? 'Bulgaria' : 'България'}
+              </p>
             </div>
           </div>
 
-          <div className="mt-8 sm:mt-8 text-center text-xs text-gray-200 px-2">
+          <div className="mt-8 sm:mt-8 text-center text-base text-gray-200 px-2">
             © {new Date().getFullYear()} Apartments by the River. All rights reserved.
           </div>
         </div>
